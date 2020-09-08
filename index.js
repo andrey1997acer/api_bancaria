10971b12d6ab873860aca15ebbdcc4578103a451
ANDREY DELGADO ARAYA <andrey.delgado.araya@est.una.ac.cr>
2020-09-08 17:16:15 -0600

    Init proyect

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const bd = require('./src/bd/bd.js');
+const axios = require('axios');
+
+const app = express()
+app.use(express.json())
+ 
+app.get('/api/', function (req, res) {
+  res.send('Hola mundo')
+})
+
+async function getUsers(){
+    try {
+        const users = await axios.get('https://jsonplaceholder.typicode.com/users')
+        return users;
+    } catch (error) {
+        return error;
+    }
+}
+
+app.get('/api/user', async (req, res)=>{
+    res.status(200).send(bd);
+})
+
+app.get('/api/user/:id',async (req, res)=>{
+    const id = req.params.id;
+    await bd.forEach(user=>{
+        if(user.id === id){
+            res.status(200).send(user);
+        }
+    })
+    res.status(200).send({});
+})
+
+app.post('/api/login', (request, response)=>{
+   const email = request.body.email;
+   const pass = request.body.password;
+
+    bd.forEach((user)=>{
+       if(user.email === email && user.password === pass){
+        response.status(200).send({
+            status:true,
+            token: user.token
+        })
+       }else{
+        response.status(505).send({
+            status:false,
+            login: "Email and/or password incorrect"
+        })
+       }
+   })
+})
+
+app.post('/api/trasaction', (req, res)=>{
+
+    res.send('Desde la transaccion')
+})
+
+
+
+ 
+app.listen(4000, (res, err)=>{
+    if(err){
+        console.log('No se pudo iniciar el servidor');
+    }else{
+        console.log('El servidor esta corriendo el puerto 4000');
+    }
+})
---
+++ src/bd/bd.js
@@ -0,0 +1,34 @@
+const BDusuario = [
+    {
+        id: '1',
+        email: 'andreyfdelgado@gmail.com',
+        password:'0000',
+        token:"1SD5G1S65D1G651DF56S1DC5SD1V56S1DVF51DS5"
+    },
+    {
+        id: '2',
+        email: 'andreydelgado5@gmail.com',
+        password:'0000',
+        token:'1WEF5S1DFV531SDV53S1DVC5WE1FV51SEF51WF'
+    },
+    {
+        id: '3',
+        email: 'btrs@gmail.com',
+        password:'0000',
+        token:'W1EF5W1EF56W1F5W1F3W5Q1F561WF51W53F1F515F'
+    },
+    {
+        id: '4',
+        email: 'delgado@gmail.com',
+        password:'0000',
+        token:'F1WF21SA5F1WS5F11A51SFA51F5A1W551F5Q5WF1'
+    },
+    {
+        id: '5',
+        email: 'chaves@gmail.com',
+        password:'0000',
+        token:'1WFS561QWF561QWF5WQ1Q5FD15QW1F5Q1WF5QW5F1'
+    },
+]
+
+module.exports = BDusuario;
